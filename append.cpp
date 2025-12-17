9c2cdd58c611529bb4efe229c41712353fb66277
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-17 11:13:46 +0530

    Append

---
+++ append.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ofstream out("data2.txt");
+    out << "Hello World!" << endl;
+    out << "Akhanda 2" << endl;
+    out.close();
+}
