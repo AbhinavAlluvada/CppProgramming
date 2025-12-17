27a08a0a75d859ce6677c484e3406b20c0a45ea0
AbhinavAlluvada <abhinav.alluvada@gmail.com>
2025-12-17 14:09:24 +0530

    Recursion

--- recursion.cpp
+++ recursion.cpp
@@ -1,27 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Recursion{
-    public:
-    int factorial(int n){
-        if(n == 0 || n == 1){
+
+class Recursion
+{
+public:
+    int factorial(int n)
+    {
+        if (n == 0 || n == 1)
+        {
             return 1;
         }
-        return n * factorial(n-1);
+        return n * factorial(n - 1);
     }
-    void print1ton(int n){
-        if(n == 1){
-            cout << 1 << " ";
+
+    void print1ton(int n)
+    { // ← void, not int
+        if (n == 0)
+        {
             return;
         }
-        cout << 
+        print1ton(n - 2);
+        cout << n << " ";
+    }
+    void printton(int n)
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        printton(n - 1);
+        cout << n << " ";
+        if (n % 10 == 0)
+        {
+            cout << endl;
+        }
     }
 };
-int main(){
+
+int main()
+{
     int n;
     cout << "Enter a number: ";
     cin >> n;
+
     Recursion obj;
-    int result = obj.factorial(n);
-    cout << result << endl;
+    obj.printton(n);
+
     return 0;
 }
